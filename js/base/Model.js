74c1a99cf07ed2173129a549b94c5956f4b3ca4d
zzpig233 <203360009@qq.com>
2018-04-08 15:21:33 +0800

    初始化

--- js/base/Model.js
+++ js/base/Model.js
@@ -7,8 +7,8 @@ window.Model = function(options){
   let resourceName = options.resourceName
   return {
     init: function(){
-      var APP_ID = 'TsDnap9SEXjSvGSowP7gXXJC-gzGzoHsz'
-      var APP_KEY = 'rGye31p12mM3wFpNRn9RADu9'
+      var APP_ID = 'fYbTwH7zLi4sG1XDxhUfb4Jg-gzGzoHsz'
+      var APP_KEY = 'E6tzYjA2zxbRiixYO2s8OVAJ'
       AV.init({ appId: APP_ID, appKey: APP_KEY })
     },
     fetch: function(){
